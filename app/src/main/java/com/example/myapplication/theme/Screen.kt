be8f4ce975d16086a3cc8c846965cb8f0a07f455
007guineapig <smieskojakub@gmail.com>
2024-04-12 01:12:50 +0200

    pridanie uvod interface

---
+++ app/src/main/java/com/example/myapplication/theme/Screen.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.theme
+
+sealed class Screen(val rout:String) {
+    object Menu:Screen("Menu")
+    object Recept:Screen("Recept")
+    object LastScreen:Screen("LastScreen")
+    object Uvod:Screen("Uvod")
+}
